695eafc32eebc5381104927e33fd58a20cdc7923
aamiiin <aminfoladi20@gmail.com>
2020-05-20 13:35:27 +0430

    modefid all file

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,11 @@
+
 var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "page1.html"
+        template : "<h1>Main</h1><p>Click on the links to change this content</p>"
     })
-    .when("/page2", {
-        templateUrl : "page2.html"
-    })
+    .when("/tomato", {
+        template : "<h1>Tomato</h1><p>Tomatoes contain around 95% water.</p>"
+    });
 });
