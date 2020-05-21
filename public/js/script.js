b0ddb02d06ae1a555fdabbce96c9c8e8e08b1388
aamiiin <aamiiinf@gmail.com>
2020-05-21 12:58:40 +0430

    modifaid script.js

--- public/js/script.js
+++ public/js/script.js
@@ -3,9 +3,9 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        template : "<h1>Main</h1><p>Click on the links to change this content</p>"
+        templateurl : "page2.html"
     })
     .when("/tomato", {
-        template : "<h1>Tomato</h1><p>Tomatoes contain around 95% water.</p>"
+        templateUrl : "page1.html"
     });
 });
